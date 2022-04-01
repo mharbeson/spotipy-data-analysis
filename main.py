4c4d15d72d701df924ab03bff760ab34343aad85
Matt Harbeson <matthew.harbeson@gmail.com>
2022-03-31 20:49:42 -0400

    ad

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import os
 import sys
 import spotipy
-# import webbrowser
 import pandas as pd
 import spotipy.util as util
 from json.decoder import JSONDecodeError
-# from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 
 # Load environmental variables from .env file
@@ -25,11 +23,9 @@ def get_key_env(key_name):
     key_value = os.getenv(key_name)
     return key_value
 
-
-# SPOTIPY_CLIENT_ID = get_key_env('SPOTIPY_CLIENT_ID')
-# SPOTIPY_CLIENT_SECRET = get_key_env('SPOTIPY_CLIENT_SECRET')
-# SPOTIPY_REDIRECT_URI = get_key_env('SPOTIPY_REDIRECT_URI')
-
+SPOTIPY_CLIENT_ID = get_key_env('SPOTIPY_CLIENT_ID')
+SPOTIPY_CLIENT_SECRET = get_key_env('SPOTIPY_CLIENT_SECRET')
+SPOTIPY_REDIRECT_URI = get_key_env('SPOTIPY_REDIRECT_URI')
 
 # Erase cache and prompt for user permission
 try:
@@ -41,6 +37,7 @@ except (AttributeError, JSONDecodeError):
 # Create our spotify object with permissions
 spotifyObject = spotipy.Spotify(auth=token)
 
+''' Returns the ID number of a track'''
 def getTrackId(saved_songs):
     for item in saved_songs['items']:
         track = item['track']
@@ -48,12 +45,12 @@ def getTrackId(saved_songs):
     return id
 
 
-# Return total number of songs in users saved tracks
+''' Return total number of songs in users saved tracks '''
 def getUserSavedSongsTotal():
     tempSong = spotifyObject.current_user_saved_tracks(limit=1)
     return tempSong['total']
 
-
+''' Returns list of current users liked songs. Currently rate limited to the last 500 tracks liked. See comment below. '''
 def getCurrentUserSavedSongs():
     savedSongs = []
     increment = 1
@@ -72,7 +69,7 @@ def getCurrentUserSavedSongs():
     return savedSongs
 
 
-# Retrieve track identifiers from song
+''' Returns list of track features '''
 def getTrackFeatures(id):
     track_info = spotifyObject.track(id)
     features = spotifyObject.audio_features(id)
@@ -101,17 +98,29 @@ def getTrackFeatures(id):
     return track_features
 
 
-tracks = []
-recent_songs = getCurrentUserSavedSongs()
-for item in recent_songs:
-    print(f'Anlyzing track {recent_songs.index(item) + 1} of {len(recent_songs)}')
-    track = getTrackFeatures(item)
-    tracks.append(track)
+''' Generates CSV using Pandas dataframes'''
+def spotifyCSV(tracks):
+    df = pd.DataFrame(tracks, columns = ['name', 'album', 'artist', 'release_date', 'length', 'popularity', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo'])
+
+    df['release_date'] = pd.to_datetime(df['release_date'], format='%Y-%m-%d')
+
+    df.to_csv('data/spotipy.csv', sep = ',')
 
 
-# Use pandas or numpy to graph
-df = pd.DataFrame(tracks, columns = ['name', 'album', 'artist', 'release_date', 'length', 'popularity', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo'])
+def main():
+    try:
+        tracks = []
+        recent_songs = getCurrentUserSavedSongs()
+        for item in recent_songs:
+            print(f'Anlyzing track {recent_songs.index(item) + 1} of {len(recent_songs)}')
+            track = getTrackFeatures(item)
+            tracks.append(track)
+    
+        spotifyCSV(tracks)
 
-df['release_date'] = pd.to_datetime(df['release_date'], format='%Y-%m-%d')
+    except: 
+        print('Unknown Error, Please restart')
 
-df.to_csv('data/spotipy.csv', sep = ',')
+
+if __name__ == '__main__':
+    main()
